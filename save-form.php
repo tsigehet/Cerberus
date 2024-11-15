95e7909f6b09f955ce9832ff77b09034dcaf8611
Omega-oxide <omegagamer203@gmail.com>
2024-11-15 09:04:48 -0500

    idk

---
+++ save-form.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "MySQL";
+$username = "cerberus";
+$password = "";
+$dbname = "Cerberus-forms";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Insert data into database
+$sql = "INSERT INTO your_table (name, email, message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
